3f09e6baa9ff7da26a9df33e34df266b6ab2ba51
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-07-16 14:13:58 +0000

    fix db url

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["8000-alayousef-djangoblog-u3phi9e4ucb.ws-eu101.gitpod.io", 
+ALLOWED_HOSTS = ["8000-alayousef-djangoblog-u3phi9e4ucb.ws-eu101.gitpod.io",
                  "codestar-alaa-2023.herokuapp.com", "localhost"]
 
 
